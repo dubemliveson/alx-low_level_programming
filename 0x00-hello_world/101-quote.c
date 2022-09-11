362aff59ee60cd1a29247bfb59e9e0189f386135
dubemliveson <dubemnkemka@gmail.com>
2022-09-11 14:38:32 -0700

    various types

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 /**
  * main - prints
  *
- * Description: prints and that piece of art is useful" - Dora Korpar,2015-10-19 
+ * Description: prints and that piece of art is useful 
  * Return - 1
  */
 
