ace2f72cc2bc7f766c4da5c66b50a0089687ad90
dubemliveson <dubemnkemka@gmail.com>
2022-09-21 13:21:12 -0700

    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,6 +10,14 @@
 
 int main(void)
 {
-	printf("_putchar %d\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
