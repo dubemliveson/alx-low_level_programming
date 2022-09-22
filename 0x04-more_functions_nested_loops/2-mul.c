df8194689eeb6e56818ec41af05494966c4af470
dubemliveson <dubemnkemka@gmail.com>
2022-09-22 11:57:54 -0700

    neat

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multilplies two integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: product of a & b
+ */
+
+int mul(int a, int b)
+{
+	int product = a * b;
+	return (product);
+}
