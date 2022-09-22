598c11f20ae14c69dda352cdcd23692398131380
dubemliveson <dubemnkemka@gmail.com>
2022-09-22 04:55:48 -0700

    new

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - does my stuff
+ * @void - does it's thing
+ *
+ * Description: print_alphabet_x10 is a g
+ */
+
+void print_alphabet_x10
+{
+	char c, l;
+
+	for (l = 0; l <= 9; l++)
+	{
+	for (c = 'a', c <= 'z', c++)
+	{
+		putchar(c);
+	}
+		putchar('\n');
+	}
+}
