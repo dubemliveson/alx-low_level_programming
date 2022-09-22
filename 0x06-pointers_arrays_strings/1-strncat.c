2c69835a5292782121cc3f17044d3e4aef69eaa4
dubemliveson <dubemnkemka@gmail.com>
2022-09-22 15:27:15 -0700

    new

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _strncat - Concatenates  strings using at most aninputted number of bytes from src.
+ * _strncat - Concatenates  strings using at most an 
+ * nputted number of bytes from src.
  * @dest: The string to be appended upon.
  * @src: The string to be appended to dest.
  * @n: The number of bytes from src to be appended to dest.
