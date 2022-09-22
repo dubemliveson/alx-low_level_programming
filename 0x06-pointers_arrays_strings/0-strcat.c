08db7522696c37dc40ec19e54b2f01ca27dcc0b4
dubemliveson <dubemnkemka@gmail.com>
2022-09-22 00:48:26 -0700

    new

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,45 +1,26 @@
-include "main.h"                                                                                                                    
+#include "main.h"
 
-                                                                                                                                     
-
-/**                                                                                                                                  
+/**
+ * _strncpy - Copies at most an inputted number
+ * of bytes from string src into dest
+ * @dest: The buffer storing the string copy
+ * @src: The source string
+ * @n: The maximum number of bytes to copied from src
  *
- *  * strcat - Concatenates the string pointed to by @src, including the terminat                                                       
- *
- *   *          null byte, to the end of the string pointed to by @dest.                                                                 
- *
- *    * @dest: A pointer to the string to be concatenated upon.                                                                           
- *
- *     * @src: The source string to be appended to @dest.                                                                                  
- *
- *      *                                                                                                                                   
- *
- *       * Return: A pointer to the destination string @dest.                                                                                
- *
- *        */                                                                                                                                  
+ * Return: A pointer to the resulting string dest.
+ */
 
-char *strcat(char *dest, const char *src)                                                                                            
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0,
 
-{                                                                                                                                    
+	while (src[index++])
+		src_len++;
 
-	        int index = 0, dest_len = 0;                                                                                                 
-
-		                                                                                                                                     
-
-		        while (dest[index++])                                                                                                        
-
-				                dest_len++;                                                                                                          
-
-			                                                                                                                                     
-
-			        for (index = 0; src[index]; index++)                                                                                         
-
-					                dest[dest_len++] = src[index];                                                                                       
-
-				                                                                                                                                     
-
-				        return (dest);                                                                                                               
-
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+	
+	for (index = src_len, index < n; index++)
+		dest[index] = '\0';
+	return (dest);
 }
-
-
