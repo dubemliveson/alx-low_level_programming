a3ecfa5144b338b8731b409707efb34c817ba30d
dubemliveson <dubemnkemka@gmail.com>
2022-09-22 01:32:52 -0700

    new

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,7 +12,7 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, src_len = 0,
+	int index = 0, src_len = 0;
 
 	while (src[index++])
 		src_len++;
@@ -20,7 +20,7 @@ char *_strncpy(char *dest, char *src, int n)
 	for (index = 0; src[index] && index < n; index++)
 		dest[index] = src[index];
 	
-	for (index = src_len, index < n; index++)
+	for (index = src_len; index < n; index++)
 		dest[index] = '\0';
 	return (dest);
 }
