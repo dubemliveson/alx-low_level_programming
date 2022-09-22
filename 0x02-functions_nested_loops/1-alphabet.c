677bbafc7e37fbb6309e1f528af9d1e17308079d
dubemliveson <dubemnkemka@gmail.com>
2022-09-22 03:56:39 -0700

    new

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,14 @@
 
 /**
  * main - new doings
- *
+ * Description: writes alpahbets in lowercase
  * Result:0;
  */
 
 void print_alphabet(void)
 {
 	char c;
+
 	for (c = 'a'; c <= 'z'; c++)
 	{
 	putchar(c);
