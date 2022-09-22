19497dd216d9c698f809c0dbcd9a98e46953c5e9
dubemliveson <dubemnkemka@gmail.com>
2022-09-22 04:14:26 -0700

    new

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,10 +4,10 @@
  * main - new doings
  * Description: writes alpahbets in lowercase with function print_alphabet
  *
- * print_alphabet - function to print lowercase alphabets
- * @a: void
- * @b: prints alphabets
+ * void print_alphabet - function to print lowercase alphabets
+ * @void: does the job
  *
+ * Description: new stuff
  * Result:0;
  */
 
