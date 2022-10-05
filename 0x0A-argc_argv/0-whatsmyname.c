de93935d868d7a9ce1d30b8e44370a03f151f15e
dubemliveson <dubemnkemka@gmail.com>
2022-10-05 07:16:14 -0700

    new2

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
  /**
-  * main - Print the name of the program
-  * @argc - Count arguments
+  * main - it does what is does and yet teh function is
+  * @argc: Count arguments
   * @argv: Arguments
   *
   * Return: Always 0 (Success)
@@ -10,7 +10,7 @@
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
-printf("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 
-return (0);
+	return (0);
 }
