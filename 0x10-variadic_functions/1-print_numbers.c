750f18fdf07768addf5f2fb98968fcd8b5e4a8b2
dubemliveson <dubemnkemka@gmail.com>
2022-10-13 17:30:30 -0700

    new

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,7 +4,7 @@
 
 /**
  * print_numbers - Prints numbers, followed by a new line.
- * @seperator: The string to be printed between numbers.
+ * @separator: The string to be printed between numbers.
  * @n: The number of integers passed to the function.
  * @...: A variable number of numbers to be printed.
  */
